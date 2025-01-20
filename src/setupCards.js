fc1618752b7826a12c776ab94a7b77079c3a06a1
Jonas Manzl <jonasmanzl99@web.de>
2025-01-20 14:32:13 +0100

    SVG Grafiken eingebunden, remove() funktioniert noch nicht

--- src/setupCards.js
+++ src/setupCards.js
@@ -1,18 +1,57 @@
 import { store } from './store'
+import cardSvg from '@/assets/card.svg'
 
 export function generateCards(selectedCategory, selectedMode, numberOfCards) {
-  store.selectedMode = selectedMode
-  store.selectedCategory = selectedCategory
-  store.numberOfCards = numberOfCards
 
-  const cards = Array.from({ length: numberOfCards }, (_, index) => ({
-    id: index, //Identifikator der Karte
-    value: `Karte ${index + 1}`, //Wert der Karte hier müsste man dann das Bild einfügen
-  }))
+  store.selectedMode = selectedMode;
+  store.selectedCategory = selectedCategory;
+  store.numberOfCards = numberOfCards;
 
-  //die Karten werden gemischt und sowohl die korrekte als auch die start Reihenfolge wird gespeichert
-  store.correctCards = cards.slice()
-  store.cards = cards
-  store.cards = store.cards.sort(() => Math.random() - 0.5)
-  store.startingCards = cards.slice()
+  fetch(cardSvg)
+    .then(response => response.text())
+    .then(svgContent => {
+      const parser = new DOMParser();
+      const svgDocument = parser.parseFromString(svgContent, 'image/svg+xml');
+      let svgTemplate = svgDocument.documentElement;
+
+      const min = 1;
+      const max = 28;
+      let cards = [];
+      cards[0] = {id: 1, svg: svgTemplate.cloneNode(true)};
+
+
+      for (let i = 1; i < store.numberOfCards; i++) {
+        console.log(i);
+        let oldCard = cards[i - 1];
+        let oldSvg = oldCard.svg.cloneNode(true);
+        let newSvg = updateSvgID(oldSvg, 'card'+ i);
+        //newSvg = removePart(getRandomInt(min, max), newSvg);
+        cards[i] = {id: i, svg: newSvg};
+        console.log(i);
+      }
+      store.cards = cards;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    }
+  );
+}
+
+function removePart(id, svgContent) {
+  svgContent.getElementById(id).remove();
+
+  return svgContent;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateSvgID(svgContent, newId) {
+  if ( svgContent.hasAttribute("id")) {
+    svgContent.removeAttribute("id");
+  }
+  svgContent.setAttribute("id", newId);
+
+  return svgContent;
 }
