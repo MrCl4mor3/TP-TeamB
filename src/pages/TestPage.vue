c25e9b74cc9a7786433180622c2b79ca59b3e169
Jonas Manzl <jonasmanzl99@web.de>
2025-02-01 14:43:29 +0100

    Toast testweise auf die Testpage implementiert

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,39 +1,28 @@
 <template>
-  <div class="p-4">
-    <Button label="Öffne Dialog" icon="pi pi-external-link" @click="openDialog" />
-
-    <Dialog v-model:visible="visible" modal header="Dynamischer Dialog" :style="{ width: '400px' }">
-      <p>Hier ist ein dynamischer Dialog mit PrimeVue.</p>
-      <Button label="Schließen" icon="pi pi-times" @click="visible = false" class="p-button-text" />
-    </Dialog>
+  <div class = "test-page">
+    <Toast/>
+    <ButtonPress @click="showToast">Zeige Toast</ButtonPress>
   </div>
 </template>
 
-<script>
-import Dialog from 'primevue/dialog'
-import Button from 'primevue/button'
-
-export default {
-  components: {
-    Dialog,
-    Button,
-  },
-  data() {
-    return {
-      visible: false,
-    }
-  },
-  methods: {
-    openDialog() {
-      this.visible = true
-    },
-  },
-}
-</script>
-
 <style>
-/* Optional: Stil für das Layout */
-.p-4 {
-  padding: 1rem;
+.test-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 </style>
+
+<script setup>
+import { useToast } from "primevue/usetoast";
+import Toast from "primevue/toast";
+
+const toast = useToast();
+
+const showToast = () => {
+  console.log('Toast wurde angezeigt');
+  toast.add({ severity: 'success', summary: 'Erfolg', detail: 'Toast wurde angezeigt' });
+}
+
+</script>
