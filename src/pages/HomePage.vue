90d3984ea925f4f8ed60ce5b530789df11acfc28
Jonas Manzl <jonasmanzl99@web.de>
2025-02-08 20:51:20 +0100

    Dialog in eigene Vue Komponente ausgelagert.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,6 +8,7 @@ import descriptions from '../descriptions/homePageDescriptions.json'
 import router from '@/router.js'
 import Slider from 'primevue/slider'
 import Dialog from "primevue/dialog";
+import TutorialDialog from '@/components/TutorialDialog.vue'
 
 // Reset des Stores
 resetStore()
@@ -116,34 +117,7 @@ onBeforeUnmount(() => {
       class="dialog"
     >
       <div class="dialog-content">
-        <h2>Willkommen bei SortLab!</h2>
-        <p>Hier kannst du spielerisch lernen, wie verschiedene Sortieralgorithmen funktionieren. SortLab hilft dir, das Konzept von Algorithmen und Computational Thinking besser zu verstehen.</p>
-
-        <h2>Setup – So startest du:</h2>
-        <ul>
-          <li><strong>Freies Sortieren:</strong> Hier kannst du die Karten beliebig aufdecken und vertauschen, ohne einem bestimmten Algorithmus zu folgen.</li>
-          <li><strong>Vorgegebenes Sortieren:</strong> Wähle zusätzlich einen Algorithmus und folge dessen Regeln, um die Karten zu sortieren.</li>
-          <li>Entscheide, wie viele Karten du sortieren möchtest.</li>
-        </ul>
-
-        <h2>Wie funktioniert das Sortieren?</h2>
-        <p>Die Karten enthalten Bilder mit Strichen. "Kleiner" bedeutet, dass eine Karte weniger Striche hat als eine andere. Deine Aufgabe ist es, die Karten in der richtigen Reihenfolge zu sortieren.</p>
-        <p>Hierfür kannst du bis zu zwei aufdecken und nachdem vergleichen vertauschen. Bei Mergesort und Quicksort gibt es algorithmenspezifische Zusatzfunktionen.</p>
-        <!-- Beispielkarten mit "<" einfügen -->
-
-        <h2>Hilfreiche Funktionen:</h2>
-        <ul>
-          <li>Falls du nicht mehr weißt, wie dein gewählter Algorithmus funktioniert, kannst du mit dem ?-Symbol oben rechts eine Erklärung abrufen.</li>
-          <li>Mit dem ↻-Symbol kannst du die Karten neu mischen und noch einmal von vorne starten.</li>
-        </ul>
-
-        <h2>Punkte (Score):</h2>
-        <p>Dein Score zählt, wie oft du eine Karte aufdeckst. Je effizienter du sortierst, desto besser!</p>
-
-        <h2>Vergleich mit festen Algorithmen:</h2>
-        <p>Wenn du fertig bist und auf "Fertig sortiert" geklickt hast, kannst du deinen Score mit dem der festen Algorithmen vergleichen.</p>
-
-        <p>Viel Spaß mit SortLab!</p>
+        <TutorialDialog />
       </div>
     </Dialog>
 
